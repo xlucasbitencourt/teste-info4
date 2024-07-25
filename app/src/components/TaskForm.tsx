5ed33ffb9845aea067f496e34ee794144ba97d10
Lucas Bitencourt <lblukz@gmail.com>
2024-07-25 14:23:50 -0300

    feat: Update TaskForm component to handle form submission and state management

--- app/src/components/TaskForm.tsx
+++ app/src/components/TaskForm.tsx
@@ -1,24 +1,52 @@
 import { Button, Label, TextInput, Textarea } from "flowbite-react";
 
-type Props = {};
+type Props = {
+  title: string;
+  setTitle: (title: string) => void;
+  description: string;
+  setDescription: (description: string) => void;
+  handleSubmit: (e: React.FormEvent) => void;
+};
 
-export default function TaskForm({}: Props) {
+export default function TaskForm({
+  title,
+  setTitle,
+  description,
+  setDescription,
+  handleSubmit,
+}: Props) {
   return (
-    <form className="flex max-w-lg flex-col gap-4 mx-auto">
+    <form onSubmit={handleSubmit} className="flex max-w-lg flex-col gap-4 mx-auto">
       <div>
         <div className="mb-2 block">
           <Label htmlFor="title" value="Title" />
         </div>
-        <TextInput id="title" type="text" placeholder="Title..." required />
+        <TextInput
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+          id="title"
+          type="text"
+          placeholder="Title..."
+          required
+        />
       </div>
       <div>
         <div className="mb-2 block">
           <Label htmlFor="description" value="Description" />
         </div>
-        <Textarea id="description" placeholder="Describe your task..." required rows={4} />
+        <Textarea
+          value={description}
+          onChange={({ target }) => setDescription(target.value)}
+          id="description"
+          placeholder="Describe your task..."
+          required
+          rows={4}
+        />
       </div>
 
-      <Button type="submit" size="lg" gradientMonochrome="lime">Create</Button>
+      <Button type="submit" size="lg" gradientMonochrome="lime">
+        Create
+      </Button>
     </form>
   );
 }
--- app/src/pages/Create.tsx
+++ app/src/pages/Create.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+import { createTask } from "../api";
 import TaskForm from "../components/TaskForm";
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await createTask({ title, description });
+    console.log(response);
+    setTitle("");
+    setDescription("");
+  };
+
   return (
     <div>
       <div className="flex flex-col items-center gap-2 mb-5">
         <h1 className="text-4xl font-bold">Create task</h1>
         <p className="text-3xl">Create your task</p>
       </div>
-      <TaskForm />
+      <TaskForm
+        title={title}
+        setTitle={setTitle}
+        description={description}
+        setDescription={setDescription}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 }
